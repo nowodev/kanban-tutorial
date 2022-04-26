64631ac2ee862312ec99dfc97a348935269f2ec1
Favour <toluwaniokunowo@gmail.com>
2022-04-26 02:51:50 +0100

    add Task component

--- src/components/BoardColumn.vue
+++ src/components/BoardColumn.vue
@@ -2,27 +2,25 @@
     <div>
         <div class="uk-card uk-card-default uk-card-body" id="todo-col">
             <h3>{{ name }}</h3>
-            <div class="uk-alert-default" uk-alert>
-                <p>Learn Node</p>
-                <div class="has-text-centered icons">
-                    <button class="uk-button uk-button-primary uk-button-small">
-                        Edit
-                    </button>
-                    <span style="margin:2px">
-                    </span>
-                    <button class="uk-button uk-button-danger uk-button-small">
-                        Delete
-                    </button>
-                </div>
-            </div>
+
+            <Task />
         </div>
     </div>
 </template>
 
 <script>
+import Task from "./Task.vue";
 export default {
     name: "BoardColumn",
-    
-    props: ["name"]
+
+    props: ["name"],
+
+    components: { Task }
 }
 </script>
+
+<style scoped>
+.uk-card-body {
+    padding: 12px 30px;
+}
+</style>
